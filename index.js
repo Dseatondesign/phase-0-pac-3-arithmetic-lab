b1b9352da20391fcd2bb39aaff3ddb5e1cb9336d
Dane <Dseatondesign@gmail.com>
2022-07-16 22:26:36 +1000

    Completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+// basic math functions
+function add() {
+    return x + y;
+  }
+function subtract() {
+    return x - y;
+}
+function multiply() {
+    return x * y;
+}
+function divide() {
+    return x / y;
+}
+function add(a, b) {
+    return a + b;
+  }
+function subtract(b, a) {
+    return b - a;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+// incrementor and decrementor operations
+function increment(a) {
+    return a + 1
+}
+
+function decrement(a) {
+    return a - 1
+}
+// integers
+function makeInt(a){
+    return parseInt(a,10)
+}
+// preserveDecimal
+function preserveDecimal(a){
+    return parseFloat(a)
+}
+
